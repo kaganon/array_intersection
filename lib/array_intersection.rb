ad0cf95c0f8d9224ea8bb140d308b4c027bac5da
Katrina Aganon <katrina.aganon@gmail.com>
2018-10-29 19:32:13 -0700

    implement array intersection with brute force

--- lib/array_intersection.rb
+++ lib/array_intersection.rb
@@ -1,4 +1,30 @@
+# array intersection
+
+
 # Creates a new array to return the intersection of the two input arrays
 def intersection(array1, array2)
+
+  if array1 == nil || array2 == nil
+    return []
+  end
+
+  intersection = []
+
+  array1.each do |array1_num|
+    array2.each do |array2_num|
+      if array1_num == array2_num
+        intersection << array1_num
+      end
+    end
+  end
+
+  return intersection
+
   raise NotImplementedError
 end
+
+=begin
+Time complexity = O(n1 * n2), where n1 and n2 are the lengths of the first and second array.
+
+Space complexity = O(n), where n is the smallest array length
+=end
